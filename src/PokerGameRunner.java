ad374cd3793cc55f89c59e818d9b19aa04ae26ea
ArtinL <43322185+ArtinL@users.noreply.github.com>
2020-03-05 18:09:27 -0800

    Update PokerGameRunner.java

--- src/PokerGameRunner.java
+++ src/PokerGameRunner.java
@@ -1,7 +1,7 @@
 public class PokerGameRunner {
     public static void main(String[] args) {
 
-        //printWelcomeAndRules(); //Comment this out if you need to.
+
 
         do {
             int numPlayers = getPlayerInput();
@@ -30,18 +30,7 @@ public class PokerGameRunner {
         System.out.println("Thank you for playing Poker.\nGoodbye!");
     }
 
-    static void printWelcomeAndRules() {
-        System.out.println("Hello and welcome to Poker!\n" +
-                "\n" +
-                "If you are unfamiliar with the game of poker, switch over to Chrome and take a minute to read the WikiHow article I have open\n" +
-                "I have also made a guide on how to play my version of the game on the paper next to the laptop. Refer to it as you are playing\n\n" +
-                "Please note that I made some slight modifications to the actual game of Texas Hold'em because the whole game is pretty big\n" +
-                "Minus a few things (like side pots) I had to cut out, the game is fully playable and fully functional.\n\n" +
-                "Anyway, I hope by this point you've at least somewhat familiarized yourselves with the game.\n" +
-                "There are no stakes involved, so you only need to know how the basics in order to play this\n\n" +
-                "Also, this text is being printed by a single subroutine at the top of the PokerGameRunner class in main. You can comment it out if you want\n\n" +
-                "Anyway, lets get started\n\n");
-    }
+   
 
     static int getPlayerInput() {
         int numPlayers;
